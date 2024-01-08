be7605241549c355191f8a7151836e1b23b5198f
Richard Arthur <richardarthur228@gmail.com>
2024-01-07 18:42:26 -0600

    add Editor toolbar

---
+++ components/EditorToolbar.jsx
@@ -0,0 +1,58 @@
+import {
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
+  Bold,
+  Code,
+  Italic,
+  Underline,
+} from "lucide-react";
+import React from "react";
+
+const tools = [
+  {
+    type: "BOLD",
+    icon: <Bold size={20} />,
+  },
+  {
+    type: "UNDERLINE",
+    icon: <Underline size={20} />,
+  },
+  {
+    type: "ITALIC",
+    icon: <Italic size={20} />,
+  },
+  //   {
+  //     type: "CODE",
+  //     icon: <Code size={20} />,
+  //   },
+  //   {
+  //     type: "LEFT",
+  //     icon: <AlignLeft size={20} />,
+  //   },
+  //   {
+  //     type: "RIGHT",
+  //     icon: <AlignRight size={20} />,
+  //   },
+];
+
+function EditorToolbar({ onEditorChange }) {
+  const handleClick = (type) => {
+    onEditorChange(type);
+  };
+  return (
+    <div className="flex items-center justify-center space-x-3 border p-2 bg-gray-400/10">
+      {tools.map((item) => (
+        <ToolbarIcon onClick={() => handleClick(item.type)} key={item.type}>
+          {item.icon}
+        </ToolbarIcon>
+      ))}
+    </div>
+  );
+}
+
+export default EditorToolbar;
+
+const ToolbarIcon = ({ children, ...props }) => (
+  <button {...props}>{children}</button>
+);
