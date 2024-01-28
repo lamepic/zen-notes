2c122f21676937c1c9db98fafd6e0ea475a0b769
Richard Arthur <richardarthur228@gmail.com>
2024-01-28 11:29:01 -0600

    fixed auth lag

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -2,14 +2,22 @@
 import GoogleIcon from "@/components/GoogleIcon";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/lib/auth";
+import { cn } from "@/lib/utils";
 
 function Login() {
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, authLoading } = useAuth();
 
   return (
     <div className="flex flex-col items-center h-full">
-      <div className="flex space-y-6 flex-col justify-center items-center mt-40 max-w-xl lg:w-3/12 rounded-md p-5 border border-gray-500/40 h-1/6">
-        <div className="w-full">
+      <div
+        className={cn(
+          "flex space-y-6 flex-col justify-center items-center mt-40 max-w-xl lg:w-3/12 rounded-md p-5 border-gray-500/40 h-1/6",
+          !authLoading && "border"
+        )}
+      >
+        {authLoading ? (
+          <p className="text-4xl font-bold">Loading...</p>
+        ) : (
           <Button
             className="w-full p-6 flex items-center gap-2"
             onClick={() => {
@@ -19,7 +27,7 @@ function Login() {
             <GoogleIcon className="w-8 h-8" />
             Sign In With Google
           </Button>
-        </div>
+        )}
       </div>
     </div>
   );
