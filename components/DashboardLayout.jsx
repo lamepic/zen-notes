fe77e3b48a72d1b3d4d917c12814173541b0393c
Richard Arthur <richardarthur228@gmail.com>
2024-02-22 19:58:04 -0600

    feat: add dashboard layout

---
+++ components/DashboardLayout.jsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+
+import { useNotes } from "@/lib/NotesProvider";
+import DashboardSidebar from "./DashboardSidebar";
+
+function DashboardLayout({ children }) {
+  const { showSidebar, setShowSidebar } = useNotes();
+  return (
+    <div className="h-full lg:border dark:border-gray-500 lg:w-10/12 lg:h-5/6 m-auto rounded-md lg:flex relative">
+      <div
+        className={cn(
+          "absolute lg:relative w-full h-full lg:-translate-x-0 top-0 left-0 border-r dark:border-gray-500 lg:flex-[0.23] z-20 transition-all duration-200 ease-in-out",
+          !showSidebar ? "-translate-x-full" : "bg-[var(--background)]" //
+        )}
+      >
+        <DashboardSidebar setOpenSidebar={setShowSidebar} />
+      </div>
+      <div className="lg:flex-[0.77]">{children}</div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
