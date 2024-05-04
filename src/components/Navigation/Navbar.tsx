ae736cf9fae3b430221d3e75e8588bdfd0618a69
Richard Arthur <richardarthur228@gmail.com>
2024-05-04 11:20:17 -0500

    remove sigin button

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -6,11 +6,8 @@ import ThemeToggle from '../ThemeToggle';
 function Navbar() {
     return (
         <nav className="flex justify-between p-5 pt-4 lg:px-8 items-center">
-            <Logo link={true} />
+            <Logo />
             <div className="flex items-center gap-2">
-                <Button variant="outline" className="font-semibold">
-                    <Link href="/login">Sign in</Link>
-                </Button>
                 <ThemeToggle />
             </div>
         </nav>
